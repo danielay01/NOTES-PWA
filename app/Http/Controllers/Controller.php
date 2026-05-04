88ef7a9a1862be6e1029de92dfdde967c2177efc
unknown <200082353+danielay01@users.noreply.github.com>
2026-05-04 16:40:30 +0800

    Lab7 Commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Note;
+use Illuminate\Support\Facades\Auth;
 
 abstract class Controller
 {
-    //
+    public function dashboard()
+{
+    $user = Auth::user();
+ 
+    if ($user->role == 1) {
+    
+        $notes = Note::with('user')->latest()->get();
+        return view('admin.dashboard', compact('notes'));
+    }
+ 
+    $notes = $user->notes;
+    return view('dashboard', compact('notes'));
+}
+
+
 }
